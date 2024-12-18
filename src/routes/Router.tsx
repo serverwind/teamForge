f1cba034a99144627570ba9cd54918a60f8a0d0d
lestrangeqq <lestrangeqq@gmail.com>
2024-12-18 20:35:07 +0200

    hotfix

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -3,8 +3,6 @@ import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
 import { Teams } from '../pages/Teams/Teams';
 import { SignUp } from '../pages/SignUp/SignUp';
-import { SignIn } from '../pages/SignIn/SignIn';
-
 
 export const AppRoutes = () => {
   return (
@@ -12,7 +10,6 @@ export const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<PrivateRoute><Teams /></PrivateRoute>} />
         <Route path="/sign-up" element={<PublicRoute><SignUp /></PublicRoute>} />
-        <Route path="/sign-in" element={<PublicRoute><SignIn /></PublicRoute>} />
       </Routes>
     </BrowserRouter>
   );
