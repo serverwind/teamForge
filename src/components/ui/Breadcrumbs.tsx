30fef1882d1226acdb87aacb684fb63678012e17
lestrangeqq <lestrangeqq@gmail.com>
2025-02-22 12:17:13 +0200

    Add router Link

--- src/components/ui/Breadcrumbs.tsx
+++ src/components/ui/Breadcrumbs.tsx
@@ -1,15 +1,19 @@
+import { Link } from "react-router-dom";
+
 interface BreadcrumbsProps {
-  link: string
-  text: string
-  current: string
+  link: string;
+  text: string;
+  current: string;
 }
 
 export const Breadcrumbs = ({ link, text, current }: BreadcrumbsProps) => {
   return (
     <div className="py-6 px-10">
-      <a href={link} className="text-sm text-darkRed">{text}</a>
+      <Link to={link} className="text-sm text-darkRed">
+        {text}
+      </Link>
       <span className="text-sm text-lightGray mx-1">/</span>
       <span className="text-sm text-darkRed">{current}</span>
     </div>
-  )
-}
+  );
+};
