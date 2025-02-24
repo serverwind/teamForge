0384fd4261713bd2f1d930442b7b27705af4dc42
lestrangeqq <lestrangeqq@gmail.com>
2025-02-24 16:31:49 +0200

    Hamburger menu

---
+++ src/components/ui/Hamburger.tsx
@@ -0,0 +1,5 @@
+import hamburger from "../../assets/hamburger.svg"
+
+export const Hamburger = () => {
+  return <img className="cursor-pointer sm:hidden absolute left-4" src={hamburger} alt="Menu" />
+}
--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,9 +1,11 @@
 import logo from "../../assets/logo.svg";
 import profileImg from "../../assets/profile.svg";
+import { Hamburger } from "../ui/Hamburger";
 
 export const Header = () => {
   return (
-    <header className="bg-white shadow-inner py-4 sm:px-12 flex sm:justify-between justify-center">
+    <header className="bg-white shadow-inner py-4 sm:px-12 flex sm:justify-between justify-center items-center relative">
+      <Hamburger />
       <img src={logo} alt="TeamForge Logo" />
       <div className="hidden sm:flex items-center gap-4">
         <span className="ml-2 text-darkGrey text-sm">Alex Lestra</span>
