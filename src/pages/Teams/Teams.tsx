9239e6b305fbdebb73832a5c31ec7c03f7f7646d
lestrangeqq <lestrangeqq@gmail.com>
2025-03-12 10:22:09 +0200

    Team cards styling

--- src/pages/Teams/Teams.tsx
+++ src/pages/Teams/Teams.tsx
@@ -26,10 +26,10 @@ export const Teams = ({ teams }) => {
             {teams.map((team) => (
               <div>
                 <div className="bg-gradientGray rounded-t text-center py-12">
-                  <img src={team.logo} alt={team.name} className="w-36 h-36 mx-auto" />
+                  <img src={team.logo} alt={team.name} className="sm:w-36 sm:h-36 w-16 h-16 mx-auto" />
                 </div>
-                <div className="bg-dark text-center px-2 py-4 sm:py-6 rounded-b">
-                  <div key={team.id} className="text-sm text-white font-bold mb-2">
+                <div className="bg-dark text-center px-2 py-4 sm:py-6 rounded-b h-24 flex flex-col gap-2 justify-center">
+                  <div key={team.id} className="text-sm text-white font-bold">
                     {team.name}
                   </div>
                   <div className="text-xs text-lightGray">Year of foundation: {team.year}</div>
