7319bde6bc4d2a84592de92462bc2a93631000b9
lestrangeqq <lestrangeqq@gmail.com>
2025-03-14 20:45:36 +0200

    Team Page

--- src/pages/Team/Team.tsx
+++ src/pages/Team/Team.tsx
@@ -3,10 +3,31 @@ import { Sidebar } from "../../components/ui/Sidebar";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { Breadcrumbs } from "../../components/ui/Breadcrumbs";
 
 interface Team {
   id: number;
   name: string;
+  year: string;
+  conference: string;
+  division: string;
+  logo: string;
+}
+
+interface Player {
+  id: number;
+  name: string;
+  age: number;
+  teamId: number;
+  height: string;
+  weight: string;
+  number: string;
+  photo: string;
+  position: string;
+}
+
+interface Players {
+  players: Player[];
 }
 
 interface Teams {
@@ -16,6 +37,9 @@ interface Teams {
 export const Team = () => {
   const { id } = useParams();
   const team = useSelector((state: { teams: Teams }) => state.teams.teams.find((team: Team) => team.id === Number(id)));
+  const players = useSelector((state: { players: Players }) => {
+    return state.players.players.filter((player: Player) => player.teamId === Number(id));
+  })
 
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
@@ -32,8 +56,61 @@ export const Team = () => {
       <Header toggleSidebar={toggleSidebar} />
       <section className="sm:grid sm:grid-cols-[10%_90%] h-full">
         <Sidebar toggleSidebar={toggleSidebar} isOpen={isSidebarOpen} />
-        <div className="my-4 sm:my-6 sm:mx-16 sm-4 sm:py-6 px-2 sm:px-10">{team.name}</div>
-        <section className="grid grid-cols-2 sm:grid-cols-3 gap-4 my-4"></section>
+
+        <div className="my-4 sm:my-6 sm:mx-16 sm-4 sm:py-6 sm:px-2 sm:px-10">
+        <div>
+          <div className="bg-white py-4 px-2">
+            <Breadcrumbs link="/teams" text="Teams" current={team.name} />
+          </div>
+          <div className="bg-gradientGray text-white text-center flex flex-col sm:grid sm:grid-cols-[40%_60%] sm:text-left sm:justify-center sm:items-center gap-9 py-14">
+            <img src={team.logo} alt={team.name} className="sm:w-40 w-24 mx-auto" />
+            
+          <div>
+            <h1 className="text-lg sm:text-3xl font-bold my-4">{team.name}</h1>
+            <div className="sm:grid sm:grid-cols-2">
+            <div><p className="text-lg font-bold mb-4">Year of foundation</p><p className="text-sm">{team.year}</p></div>
+            <div><p className="text-lg font-bold mb-4">Conference</p><p className="text-sm">{team.conference}</p></div>
+            <div><p className="text-lg font-bold mb-4">Division</p><p className="text-sm">{team.division}</p></div>
+            </div>
+          </div>
+
+          </div>
+        </div>
+        <section className="bg-white my-4 sm:rounded border-t border-r border-l border-lightGray">
+          <table className="overflow-x-auto w-full" cellPadding={8}>
+            <colgroup>
+              <col width="7%" />
+              <col width="63%" />
+              <col width="10%" />
+              <col width="10%" />
+              <col width="10%" />
+            </colgroup>
+            <thead>
+              <tr>
+                <th colSpan={6} className="text-grey text-left font-normal border-b border-lightGray px-7 py-4">Roster</th>
+              </tr>
+            </thead>
+            <tbody>
+            <tr className="border-b border-lightGray font-bold">
+              <td className="text-grey text-center text-sm">#</td>
+              <td className="text-grey text-sm">Player</td>
+              <td className="hidden sm:table-cell text-grey text-center text-sm">Height</td>
+              <td className="hidden sm:table-cell text-grey text-center text-sm">Weight</td>
+              <td className="hidden sm:table-cell text-grey text-center text-sm">Age</td>
+            </tr>
+                {players.map((player) => (
+              <tr className="border-b border-lightGray">
+                  <td className="text-grey text-center text-sm" key={player.id}>{player.number}</td>
+                  <td className="flex gap-4 items-center"><img src={player.photo} alt={player.name} className="w-12 rounded-full" /><div className="flex flex-col"><span className="text-grey text-sm">{player.name}</span><span className="text-lightGray text-xs">{player.position}</span></div></td>
+                  <td className="hidden sm:table-cell text-sm text-grey text-center">{player.height}</td>
+                  <td className="hidden sm:table-cell text-sm text-grey text-center">{player.weight}</td>
+                  <td className="hidden sm:table-cell text-sm text-grey text-center">{player.age}</td>
+              </tr>
+                ))}
+            </tbody>
+          </table>
+        </section>
+        </div>
       </section>
     </>
   );
--- src/store/playersReducer.ts
+++ src/store/playersReducer.ts
@@ -10,6 +10,7 @@ interface Player {
   weight: string;
   number: string;
   photo: string;
+  age: string;
 }
 
 interface PlayersState {
@@ -22,12 +23,12 @@ interface PlayersState {
 
 const initialState: PlayersState = {
   players: [
-    { id: 1, name: "Scoot Henderson", teamId: 1, position: "Guard", height: "6-3", weight: "206", number: "00", photo: playersLogos.p1 },
-    { id: 2, name: "Anfernee Simons", teamId: 1, position: "Guard", height: "6-3", weight: "200", number: "1", photo: playersLogos.p2 },
-    { id: 3, name: "Deandre Ayton", teamId: 1, position: "Center", height: "7-0", weight: "252", number: "2", photo: playersLogos.p3 },
-    { id: 4, name: "Matisse Thybulle", teamId: 1, position: "Guard-Forward", height: "6-5", weight: "201", number: "4", photo: playersLogos.p4 },
-    { id: 5, name: "Dalano Banton", teamId: 1, position: "Guard", height: "6-8", weight: "203", number: "5", photo: playersLogos.p5 },
-    { id: 6, name: "Deni Avdija", teamId: 1, position: "Forward", height: "6-9", weight: "240", number: "8", photo: playersLogos.p6 },
+    { id: 1, name: "Scoot Henderson", teamId: 1, position: "Guard", height: "6-3", weight: "206", number: "00", age: "32", photo: playersLogos.p1 },
+    { id: 2, name: "Anfernee Simons", teamId: 1, position: "Guard", height: "6-3", weight: "200", number: "1", age: "22", photo: playersLogos.p2 },
+    { id: 3, name: "Deandre Ayton", teamId: 1, position: "Center", height: "7-0", weight: "252", number: "2", age: "20", photo: playersLogos.p3 },
+    { id: 4, name: "Matisse Thybulle", teamId: 1, position: "Guard-Forward", height: "6-5", weight: "201", number: "4", age: "23", photo: playersLogos.p4 },
+    { id: 5, name: "Dalano Banton", teamId: 1, position: "Guard", height: "6-8", weight: "203", number: "5", age: "24", photo: playersLogos.p5 },
+    { id: 6, name: "Deni Avdija", teamId: 1, position: "Forward", height: "6-9", weight: "240", number: "8", age: "21", photo: playersLogos.p6 },
   ],
   filteredPlayers: [],
   searched: false,
