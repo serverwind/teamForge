1919a38429777cb3e3cd51413de15b9c0b982a34
lestrangeqq <lestrangeqq@gmail.com>
2025-03-31 20:20:54 +0300

    Search now work onchange

--- src/components/ui/Search.tsx
+++ src/components/ui/Search.tsx
@@ -21,11 +21,11 @@ export const Search = ({ setCurrentPage, search }: SearchProps) => {
         dispatch(search(values.search));
       }}
       validationSchema={Yup.object({
-        search: Yup.string().min(3, "At least 3 characters"),
+        search: Yup.string().min(2, "At least 2 characters"),
       })}
     >
       {(formik) => (
-        <form onSubmit={formik.handleSubmit} className="relative">
+        <form onChange={formik.handleSubmit} className="relative">
           <input placeholder="Search..." name="search" className="bg-white rounded px-4 py-2 w-full" onChange={formik.handleChange} value={formik.values.search} />
           <button type="submit" onClick={handlePageChange} className="absolute right-3 top-3">
             <img src={searchIcon} alt="search" />
