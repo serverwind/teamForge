ccd99b937afdd3104ef41aa5f15cacbfec8fa17d
lestrangeqq <lestrangeqq@gmail.com>
2025-04-01 19:55:09 +0300

    Filter is now adaptive

--- src/components/ui/SelectTeams.tsx
+++ src/components/ui/SelectTeams.tsx
@@ -62,17 +62,17 @@ export const SelectTeams = ({ teams, players }: SelectTeamsProps) => {
 
   return (
     <div className="sm:flex justify-end gap-4">
-      <div className={filter ? "flex flex-row items-center bg-white border border-lightestGrey pl-1 h-10 w-max rounded relative" : "hidden"}>
-        <div className={selectedTeams.length > 0 ? "grid grid-cols-[auto_auto_auto] gap-2" : "hidden" }>
+      <div className={filter ? "flex sm:flex-row flex-col sm:items-center bg-white border border-lightestGrey p-1 sm:pl-1 sm:py-0 sm:pr-0 mb-4 sm:mb-0 sm:h-10 min-h-10 w-full sm:w-max rounded sm:relative" : "hidden"}>
+        <div className={selectedTeams.length > 0 ? "grid sm:grid-cols-[auto_auto_auto] gap-2" : "hidden" }>
           {selectedTeams.slice(0, 2).map((team: Team) => (
             <div className="bg-red text-white p-2 h-8 rounded flex items-center gap-2" key={team.id}>
               {team.name}
-              <button value={team.id} onClick={(event) => handleRemove(event)}>×</button>
+              <button value={team.id} onClick={(event) => handleRemove(event)} className="ml-auto">×</button>
             </div>
           ))}
         </div>
-        {selectedTeams.length > 2 ? <div className="bg-red text-white p-2 h-8 rounded flex items-center gap-2 cursor-pointer" onClick={handleExpand}>...</div> : null}
-        <select onChange={(event) => handleSelect(event)} className="w-4 h-8 px-4 text-white bg-white bg-chevron bg-center bg-no-repeat cursor-pointer">
+        {selectedTeams.length > 2 ? <div className="bg-red text-white p-2 h-8 rounded flex items-center mt-2 sm:mt-0 gap-2 cursor-pointer order-last sm:order-none" onClick={handleExpand}>...</div> : null}
+        <select onChange={(event) => handleSelect(event)} className="w-full h-10 sm:w-4 sm:h-8 px-4 order-first sm:order-none text-white bg-white bg-chevron bg-right sm:bg-center bg-no-repeat cursor-pointer">
           <option value="">[Select team]</option>
           {teams.map((team) =>
             selectedTeams.includes(team) ? null : (
@@ -82,16 +82,16 @@ export const SelectTeams = ({ teams, players }: SelectTeamsProps) => {
             )
           )}
         </select>
-        <div className={isHidden ? "flex flex-row flex-wrap gap-2 bg-white border border-lightestGrey p-1 rounded absolute top-11 right-0 w-full" : "hidden"}>
+        <div className={isHidden ? "flex flex-col sm:flex-row flex-wrap gap-2 bg-white sm:border border-lightestGrey mt-2 sm:mt-0 sm:p-1 rounded sm:absolute top-11 right-0 w-full" : "hidden"}>
           {selectedTeams.slice(2).map((team: Team) => (
             <div className="bg-red text-white p-2 h-8 rounded flex items-center gap-2" key={team.id}>
               {team.name}
-              <button value={team.id} onClick={(event) => handleRemove(event)} className="cursor-pointer">×</button>
+              <button value={team.id} onClick={(event) => handleRemove(event)} className="ml-auto">×</button>
             </div>
           ))}
       </div>
       </div>
-      <button onClick={handleFilter} className="bg-red text-white px-4 py-2 h-10 rounded hover:bg-lightRed transition duration-300">{ filter ? "Hide" : "Add +" }</button>
+      <button onClick={handleFilter} className="bg-red text-white px-4 py-2 h-10 rounded hover:bg-lightRed transition duration-300 w-full sm:w-auto">{ filter ? "Hide" : "Add +" }</button>
     </div>
   );
 };
